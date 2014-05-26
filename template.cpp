b98a27c8e22e401a2fef5b9a80dcea5ab6b0a9ff
Pratik T <p@pratiktandel.com>
2014-05-25 20:16:12 -0700

    Add initial templates

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <complex> 
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+#define s(n)          scanf("%d",&n)
+#define sl(n)         scanf("%lld",&n)
+#define sf(n)         scanf("%lf",&n)
+#define fill(a,v)     memset(a, v, sizeof a)
+#define bitcount      __builtin_popcount
+#define all(x)        x.begin(), x.end()
+#define pb(z)       push_back( z )
+#define gcd           __gcd
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+int main() {
+  
+}
---
+++ template.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+import java.regex.*;
+
+public class Main {
+  public static void main(String args[]) throws Exception {
+    
+  }
+}
